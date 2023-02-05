11603f5d9a1c214d7578f952b1ab7cba20ae0a98
iamhenrika <113649340+iamhenrika@users.noreply.github.com>
2023-02-05 16:05:29 -0500

    Create 011023 - Lab.js

---
+++ 011023 - Lab.js
@@ -0,0 +1,10 @@
+/* Question 1: Sum all numbers
+Write a function called sumRange. It will take a number and return the sum of all numbers from 1 up to the number passed in.
+
+Sample: sumRange(3) returns 6, since 1 + 2 + 3 = 6.
+*/
+
+/* Question 2:
+Write a JavaScript program to get the integers in range (x, y).  Go to the editor
+Example : range(2, 9)
+Expected Output : [3, 4, 5, 6, 7, 8] */
